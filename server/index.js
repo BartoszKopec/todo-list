bdb0df0916fecde889f3734aacddbfe9c2047597
kopec.bartosz.app <kopec.bartosz.app@gmail.com>
2020-04-03 12:39:10 +0200

    Dodano ikone

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
 const serve = require('koa-static')
+const favicon = require('koa-favicon')
 const app = new Koa()
 const port = process.env.PORT || 3000
 
+app.use(favicon('client/favicon.ico'))
+
 app.use(logger())
+
 app.use(serve('client'))
 
 const userRoutes = require('./routes/users')
